9fa26182fb8b3f347cdbda22efe3b906c71d6f85
Andrew Reddin <andrew@bytepath.ca>
2020-09-18 15:13:23 -0300

    cleaned up asset loader

--- vue/Components/Loaders/AssetLoader.vue
+++ vue/Components/Loaders/AssetLoader.vue
@@ -3,83 +3,102 @@
      * Entity loader accepts src that is the path to the svg file containing the assets this entity wants.
      *
      * src is checked against a global list of downloaded svg files
-     * if src not already downloaded when requested, the entity that needed it first downloads it and shares it in its def tag
-     * the entity shares the src pathname and id of the def of the downloaded model so that the next time an asset needs
-     * something from "src" path, it can just immediately grab it because its already in the dom
+     * if src not already downloaded, the entity that first requests it will download it
+     * Any assets that ask for this file afterwards will use the version downloaded by the other component
+     * There is a global variable called loadedAssets that holds the list of loaded assets
+     * loadedAssets has the syntax { src: "id-that-loaded-this-file" } where src is a filename/url
      */
-
     const axios = require('axios');
     let loadedAssets = {};
+
     export default {
-        mixins: [],
         props: {
-            // The path to the image we want to load
+            /**
+             * The file path to the asset we need to load.
+             */
             src: {
                 type: String,
                 required: true,
             },
 
-            // The name of the asset i.e the id of the image
+            /**
+             * The ID that should be used on the loaded svg file
+             */
             asset: {
                 type: String,
                 required: true,
             }
         },
 
+        /**
+         * Check loadedAssets variable to see if someone else has already loaded the requested file. If so, we can
+         * use the <use> tag of the SVG spec to clone the tag with id = loadedAssets[this.src]
+         */
         created() {
-            /*
-            // If the static loadedAssets variable has a key that is equal to the path/src we are trying to
-            // load, someone else has already loaded it. loadAssets also holds the dom ID of the already
-            // loaded version of this asset we can use in conjunction with the SVG <use> tag to make
-            // a copy
-            */
             if (!Object.prototype.hasOwnProperty.call(loadedAssets, this.src)) {
-                this.loadAsset();
-            } else {
-                console.log("Entity previously loaded", loadedAssets);
-                this.loadedAssetId = loadedAssets[this.src];
+                return this.loadAsset();
             }
+
+            // Asset was previously loaded by someone else
+            this.loadedAssetId = loadedAssets[this.src];
         },
+
+        /**
+         * If we remove this component we need to reload any assets it loaded
+         */
         destroyed() {
             delete loadedAssets[this.src];
         },
+
         data() {
             return {
-                // The DOM id of the asset that was already loaded by someone else
+                /**
+                 * The DOM id we will pass to the <use> tag
+                 */
                 loadedAssetId: null,
 
-                // A stringified version of the SVG file grabbed via ajax
+                /**
+                 * A string version of the SVG file grabbed via ajax
+                 */
                 assetData: null,
             };
         },
 
         computed: {
+            /**
+             * Returns true if we have loaded the asset
+             */
             loaded() {
                 return (this.loadedAssetId !== null);
             },
         },
 
         methods: {
+            /**
+             * Makes an ajax request to load the :src file
+             */
             loadAsset() {
                 axios.get(this.src)
-                    .then((response) => {
-                        this.assetData = this.ensureUniqueIds(response.data);
-                        loadedAssets[this.src] = this.asset;
-                        this.loadedAssetId = this.asset;
-                        console.log("entity created");
-                        this.$emit("loaded");
-                    })
-                    .catch((error) => console.log('asset err', error));
+                .then((response) => {
+                    // Set this :src file as downloaded in the global list
+                    loadedAssets[this.src] = this.asset;
+
+                    this.loadedAssetId = this.asset;
+                    this.assetData = this.ensureUniqueIds(response.data);
+
+                    this.$emit("loaded");
+                })
+                .catch((error) => console.log('asset err', error));
             },
 
             /**
-             * Ensures that the ID of the imported SVG are unique by appending the asset id to EVERY id inside it
-             * @param {String} HTML representing a single element
+             * Ensures unique IDs in the imported SVG by appending :asset to EVERY id inside the svg
+             * @param {String} html The representing a single element
              * @return {Element}
              */
-            ensureUniqueIds(html) {
+            ensureUniqueIds(svg) {
                 var template = document.createElement('template');
-                template.innerHTML = html;
+                template.innerHTML = svg;
                 template.content.firstElementChild.querySelectorAll("[id]").forEach((element) => {
                     element.setAttribute("id", this.asset+element.id);
                 });
