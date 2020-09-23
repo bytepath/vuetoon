dd6e1643101d0731a38f2168a42124117d47d03a
Andrew Reddin <andrew@bytepath.ca>
2020-09-23 17:49:42 -0300

    added tween hex function that gives completely incorrect values currently

--- Helpers/Tween.js
+++ Helpers/Tween.js
@@ -10,9 +10,19 @@ export default class Tween {
         this.keyframe = keyframe;
     }
 
-    betweenNumbers(start, end){
+    /**
+     * Tweens between start and end with whatever numbers come along the way
+     * @param start a number
+     * @param end a number
+     * @returns {number} a number between start and end depending on keyframe location
+     */
+    number(start, end){
         if(this.keyframe >= this.endFrame){
             return end;
+        }
+
+        if(this.keyframe <= this.startFrame){
+            return start;
         }
 
         // The difference between start and end number
@@ -25,5 +35,34 @@ export default class Tween {
         let numbersPerFrame = numberDiff / frameDiff;
 
         return (this.keyframe - this.startFrame) * numbersPerFrame;
+    }
+
+    /**
+     * Tweens between start and end with only integer values
+     * @param start
+     * @param end
+     * @returns {number} an integer between start and end depending on keyframe location
+     */
+    integer(start, end){
+        return Math.ceil(this.number(start, end));
+    }
+
+    /**
+     * Tweens between the two provided hex values. Useful for colour transitions
+     * @param start some hex string Example: #000000
+     * @param end   some hex string Example: #FFFFFF
+     * @returns {number} an integer between start and end depending on keyframe location
+     */
+    hex(start, end){
+        let s = start.slice(1).match(/.{1,2}/g).map((val) => parseInt(val, 16));
+        let e = end.slice(1).match(/.{1,2}/g).map((val) => parseInt(val, 16));
+        let retval = [];
+        [0,1,2].map((i) => retval.push(Math.abs(this.integer(s[0], e[0])).toString(16).toUpperCase()));
+        console.log("retval", retval);
+        retval =  retval = "#" + retval.join("");
+        console.log(retval);
+        console.log();
+
+        return retval;
     }
 }
